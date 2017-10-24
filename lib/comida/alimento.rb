326be4d337f5856cc5f88c2f75e4b1530a002b36
Pedro Antonio Lima A <alu0100952599@ull.edu.es>
2017-10-24 22:29:37 +0100

    Prueba de constructor e instancias - EXITO

--- lib/comida/alimento.rb
+++ lib/comida/alimento.rb
@@ -1,3 +1,12 @@
 class Alimento
 
+	def initialize (nombre, pro, glu, lip)
+
+		@nombre = nombre
+		@proteinas = pro
+		@glucidos = glu
+		@lipidos = lip
+
+		@valor_energetico = 0.0
+	end
 end
--- spec/comida_spec.rb
+++ spec/comida_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 RSpec.describe Comida do
+
   it "has a version number" do
     expect(Comida::VERSION).not_to be nil
   end
@@ -10,6 +11,24 @@ RSpec.describe Comida do
   end
 
   it "Instanciar alimentos para comprobar constructor y variables de instancias para proteinas, glúcidos y lípidos" do
-	@huevo = 	Alimento.new(huevo,14.1,0.0,19.5)
+	huevo = 	Alimento.new("huevo",14.1,0.0,19.5)
+	leche = 	Alimento.new("leche",3.3,4.8,3.2)
+	yogurt = 	Alimento.new("yogurt",3.8,4.9,3.8)
+	cerdo = 	Alimento.new("cerdo",21.5,0.0,6.3)
+	ternera = 	Alimento.new("ternera",21.1,0.0,3.1)
+	pollo = 	Alimento.new("pollo",20.6,0.0,5.6)
+	bacalao = 	Alimento.new("bacalao",17.7,0.0,0.4)
+	atun = 		Alimento.new("atun",21.5,0.0,15.5)
+	salmon = 	Alimento.new("salmon",19.9,0.0,13.6)
+	aceite = 	Alimento.new("aceite",0.0,0.2,99.6)
+	chocolate =	Alimento.new("chocolate",5.3,47.0,30.0)
+	azucar =	Alimento.new("azucar",0.0,99.8,0.0)
+	lentejas =	Alimento.new("lentejas",23.5,52.0,1.4)
+	papas = 	Alimento.new("papas",2.0,15.4,0.1)
+	tomate = 	Alimento.new("tomate", 1.0,3.5,0.2)
+	cebolla = 	Alimento.new("cebolla",1.3,5.8,0.3)
+	manzana = 	Alimento.new("manzana",0.3,12.4,0.4)
+	platanos = 	Alimento.new("platanos",1.2,21.4,0.2)
+
   end
 end
